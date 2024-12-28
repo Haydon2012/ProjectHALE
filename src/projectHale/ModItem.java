ba927be7d64799802812c410aad8282c8736890d
Haydon2012 <jpsy20181002@126.com>
2024-12-28 18:52:06 +0800

    for 0.1B

--- src/projectHale/ModItem.java
+++ src/projectHale/ModItem.java
@@ -4,14 +4,22 @@ import mindustry.type.Item;
 
 public class ModItem {
     public static Item iron;
+    public static Item chip_basic;
     public static void load() {
-        iron=new Item("Iron", Color.valueOf("cfc1c1")){{
+        iron=new Item("iron", Color.valueOf("797171")){{
             hardness=1;
             cost=0.3f;
             alwaysUnlocked=true;
             flammability=0f;
             explosiveness=0f;
-            radioactivity=0.01f;
+            radioactivity=0f;
+        }};
+        chip_basic=new Item("chip_basic", Color.valueOf("a1db5e")){{
+            cost=0.5f;
+            alwaysUnlocked=true;
+            flammability=0.2f;
+            explosiveness=0.02f;
+            radioactivity=0.2f;
         }};
     }
 }
--- src/projectHale/projectHale.java
+++ src/projectHale/projectHale.java
@@ -10,24 +10,24 @@ public class projectHale extends Mod{
 
 
     public projectHale(){
-        Log.info("Loaded projectHale constructor.");
+        Log.info("Loaded projectHale constructor...");
 
         //listen for game load event
         Events.on(ClientLoadEvent.class, e -> {
             //show dialog upon startup
             Time.runTask(10f, () -> {
                 BaseDialog dialog = new BaseDialog("welcome!");
-                dialog.cont.add("-Welcome-").row();
-                dialog.cont.add("it is just in alpha...").row();
-                dialog.cont.add("Tks for your supporting!").row();
-                dialog.cont.image(Core.atlas.find("projectHale-frog")).pad(20f).row();
-                dialog.cont.button("continue...", dialog::hide).size(100f, 50f);
+                dialog.cont.image(Core.atlas.find("projecthale-ICON")).pad(30f).row();
+                dialog.cont.add("-Welcome to [project:HALE]-").row();
+                dialog.cont.add("[yellow]warning:Beta Version!").row();
+                dialog.cont.add("[sky]Tks for your supporting!").row();
+                dialog.cont.button("->continue->", dialog::hide).size(300f, 50f);
                 dialog.show();
             });
         });
     }
     public void loadContent(){
-        Log.info("Loading projectHale contents...");
+        Log.info("Loading projectHale items...");
         ModItem.load();
     }
 
