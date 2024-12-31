069658920f085b39a5a293df6e6b45e05eb09754
Haydon2012 <jpsy20181002@126.com>
2024-12-31 22:16:43 +0800

    for 0.1B

--- src/projectHale/ModBlock.java
+++ src/projectHale/ModBlock.java
@@ -1,12 +1,15 @@
 package projectHale;
 
+import mindustry.content.Blocks;
 import mindustry.content.Fx;
 import mindustry.content.Items;
 import mindustry.gen.Sounds;
 import mindustry.type.Category;
 import mindustry.type.ItemStack;
 import mindustry.world.blocks.defense.Wall;
+import mindustry.world.blocks.distribution.ArmoredConveyor;
 import mindustry.world.blocks.distribution.Conveyor;
+import mindustry.world.blocks.distribution.StackConveyor;
 import mindustry.world.blocks.production.GenericCrafter;
 import mindustry.world.meta.BuildVisibility;
 
@@ -14,7 +17,7 @@ public class ModBlock {
     public static Wall iron_wall;
     public static Wall iron_wall_large;
     public static GenericCrafter chip_stamper;
-    public static Conveyor magnetic_conveyor;
+    public static StackConveyor magnetic_conveyor;
     public static void load() {
         //walls
         iron_wall = new Wall("iron_wall") {{
@@ -30,12 +33,13 @@ public class ModBlock {
             buildCostMultiplier = 2f;
         }};
         //transport
-        magnetic_conveyor=new Conveyor("magnetic_conveyor"){{
-            requirements(Category.distribution, new ItemStack[]{new ItemStack(ModItem.iron, 6),new ItemStack(ModItem.chip_basic,3)});
+        magnetic_conveyor=new StackConveyor("magnetic_conveyor"){{
+            requirements(Category.distribution, BuildVisibility.shown, new ItemStack[]{new ItemStack(ModItem.iron, 6),new ItemStack(ModItem.chip_basic,3)});
             canOverdrive=true;
             hasItems=true;
-            speed=1f;
+            speed=0.2f;
             size=1;
+            itemCapacity=8;
         }};
         //factories
         chip_stamper = new GenericCrafter("chip_stamper"){{
